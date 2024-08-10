d5d9088c1e7666d0287e6f14a3dd4cda6ec1da40
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-08-10 09:09:51 +0000

    Remove unneeded files

--- app/index-org.js
+++
@@ -1,9 +0,0 @@
-const AWSXRay = require('aws-xray-sdk-core');
-const AWS = AWSXRay.captureAWS(require('aws-sdk'));
-
-const s3 = AWS.S3();
-
-exports.handler = async function(event) {
-    console.log('Lambda being called with: ', event);
-    return s3.listBuckets().promise();
-};
